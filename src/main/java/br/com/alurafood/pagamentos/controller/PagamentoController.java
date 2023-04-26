e079e59d0f78868555054abd090629a9ca62724c
gabrie345 <gabriel345wp@gmail.com>
2023-04-26 12:35:31 -0300

    Ajuste Controller configuração ModelMapper

--- src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
+++ src/main/java/br/com/alurafood/pagamentos/controller/PagamentoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,7 +25,7 @@ import br.com.alurafood.pagamentos.service.PagamentoService;
 
 @RestController
 @RequestMapping("/pagamentos")
-public class PagamentoColntroller {
+public class PagamentoController {
 	
 	@Autowired
 	private PagamentoService service;
@@ -41,7 +42,7 @@ public class PagamentoColntroller {
 		PagamentoDto dto = service.obterPorId(id);
 		return ResponseEntity.ok(dto);
 	}
-	
+	@PostMapping
     public ResponseEntity<PagamentoDto> cadastrar(@RequestBody @Valid PagamentoDto dto, UriComponentsBuilder uriBuilder) {
         PagamentoDto pagamento = service.criarPagamento(dto);
         URI endereco = uriBuilder.path("/pagamentos/{id}").buildAndExpand(pagamento.getId()).toUri();
