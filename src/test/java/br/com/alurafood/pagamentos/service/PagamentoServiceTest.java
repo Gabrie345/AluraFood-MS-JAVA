8047cb45db192dbc9815c7ca4f92d9062600faba
gabrie345 <gabriel345wp@gmail.com>
2023-04-26 11:29:08 -0300

    Controller Pagamento.

---
+++ src/test/java/br/com/alurafood/pagamentos/service/PagamentoServiceTest.java
@@ -0,0 +1,14 @@
+package br.com.alurafood.pagamentos.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PagamentoServiceTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
